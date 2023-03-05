3649f169938b950baac7dd904fe4bb74ecd1f985
yo9299 <yo9299@gmail.com>
2023-03-05 11:01:26 +0100

    delete repetitions

--- King.hs
+++ King.hs
@@ -22,20 +22,23 @@ kThreatenPos (i,j) = mkPosList $ filter (\ (k,l) -> k >= 1 && l >= 1 && k <= n &
 kAlgo :: Int -> Domain -> PartialSolution -> Maybe [Solution]
 kAlgo k dom partSol
     | M.null dom = Just []
+    | M.size dom < k = Just []
     | k==1 = let totalsols = fmap L.sort $ map (: partSol) (getPositions dom)
              in trace ("[leaf] found " ++ show (length totalsols) ++ " king assignments") $ 
                     if length totalsols <= 48 
                         then Just totalsols
                         else Nothing
-    | otherwise = let explore :: Position -> Maybe [Solution]
-                      explore pos = let dom' = dom M.\\ (kThreatenPos pos)
-                                        partSol' = pos:partSol
-                                    in kAlgo (k-1) dom' partSol'
+    | otherwise = let explore :: Position -> Domain -> Maybe [Solution]
+                      explore pos d = let dom' = d M.\\ (kThreatenPos pos)
+                                          partSol' = pos:partSol
+                                      in kAlgo (k-1) dom' partSol'
                       f el Nothing = Nothing
-                      f el (Just sols) = let ret = explore el
-                                             newsols = L.nub $ fromJust ret ++ sols 
-                                         in if  isNothing ret then trace ("[depth=" ++ show k ++ "] cutting") Nothing
-                                            else if length newsols <= 48
-                                                 then trace ("[depth=" ++ show k ++ "] current size= " ++ show (length newsols)) Just newsols 
-                                                 else trace ("[depth="  ++ show k ++ "] stopping king solver: " ++ show (length newsols) ++ " solutions have been enumerated.") Nothing 
-                  in foldr f (Just []) $ getPositions dom
+                      f el (Just (sols, d))  = let ret = explore el d
+                                                   newsols = L.nub $ fromJust ret ++ sols 
+                                               in if  isNothing ret then trace ("[depth=" ++ show k ++ "] cutting") Nothing
+                                                  else if length newsols <= 48
+                                                       then trace ("[depth=" ++ show k ++ "] current size= " ++ show (length newsols)) $
+                                                                Just (newsols, M.delete (posToKey el) d) 
+                                                       else trace ("[depth="  ++ show k ++ "] stopping king solver: " ++ show (length newsols) ++ " solutions have been enumerated.") 
+                                                                Nothing 
+                  in fmap fst (foldr f (Just ([], dom)) $ getPositions dom)
